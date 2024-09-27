b47a41422d5683601a4747392a08f258b6556009
Preethi grace <81759829+Preethigrace-7@users.noreply.github.com>
2024-09-27 16:01:28 +0530

    Delete Circuits/Combinational Logic/Basic Gates/Ring_or_Vibrate.v

--- Circuits/Combinational Logic/Basic Gates/Ring_or_Vibrate.v
+++
@@ -1,9 +0,0 @@
-module top_module (
-    input ring,
-    input vibrate_mode,
-    output ringer,       // Make sound
-    output motor         // Vibrate
-);
-    assign ringer=(ring==1 & vibrate_mode==0)?1:0;
-    assign motor=(ring==1 & vibrate_mode==1)?1:0;
-endmodule
