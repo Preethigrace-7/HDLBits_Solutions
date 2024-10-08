794aaa5c3e1d269a9a74531a12f48a0064378cfb
Preethi grace <81759829+Preethigrace-7@users.noreply.github.com>
2024-10-08 14:15:13 +0530

    Create Create circuit from truth table.v
    A JK flip-flop has the below truth table. Implement a JK flip-flop with only a D-type flip-flop and gates. Note: Qold is the output of the D flip-flop before the positive clock edge.
    
    J	K	Q
    0	0	Qold
    0	1	0
    1	0	1
    1	1	~Qold

---
+++ Circuits/Sequential Logic/Latches and flipflops/Create circuit from truth table.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+    always @(posedge clk)begin
+        if(!j && !k) Q<=Q;
+        if(!j && k) Q<=0;
+        if(j && !k) Q<=1;
+        if(j && k) Q<=!Q;
+    end
+endmodule
